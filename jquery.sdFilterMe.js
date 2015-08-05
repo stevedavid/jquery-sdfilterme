4de4bb49c844bcc9213afc1ccef9ed5a59994c21
Steve DAVID <stevedavid@users.noreply.github.com>
2015-08-05 22:55:32 +0200

    Create jquery.sdFilterMe.js

---
+++ jquery.sdFilterMe.js
@@ -0,0 +1,315 @@
+/*!
+ jQuery.sdFilterMe v0.1
+ (c) 2015 Steve David <http://www.steve-david.com>
+
+ MIT-style license.
+ */
+
+;(function($) {
+
+    $.fn.extend({
+        sdFilterMe: function(options) {
+            if (options && typeof(options) == 'object') {
+                options = $.extend({}, $.sdFilterMe.defaults, options);
+            }
+
+            if($(this).length == 1) {
+                new $.sdFilterMe(this, options);
+            }
+
+            return this;
+        }
+    });
+
+    $.layout = [];
+
+    $.sdFilterMe = function(el, options) {
+        var $el = $(el);
+
+        $(window).on('load', function() {
+
+            $el = $.sdFilterMe.buildLayout(el, options);
+            $.layout = $.sdFilterMe.storeCoordinates($el);
+
+            $(window).on('resize', function() {
+                $.layout = $.sdFilterMe.storeCoordinates($el);
+            });
+
+            var $boxes = $el.find('> .sdfm-inner-wrapper');
+
+            // Triggering events
+            $boxes.on('click', function() {
+                $(this).trigger('fm.boxClicked');
+            });
+
+            $(options.filterSelector).css('cursor', 'pointer').on('click', function(e) {
+                e.preventDefault();
+                $.sdFilterMe.filterBoxes($el, $(this).attr('data-filter'), options);
+            });
+
+            if(options.hoverEffect) {
+                $.sdFilterMe.hoverEffect($el, options);
+            }
+        });
+    };
+
+    $.sdFilterMe.buildLayout = function(el, options) {
+
+        var $el = $(el)
+            , $lis = $el.find('> li')
+            , widths = []
+            , heights = []
+            , clones = []
+            , maxWidth
+            , maxHeight
+            , outerWrapperId = 'sdfm-wrapper'
+            , $outerWrapper = $('<div />')
+                .attr('id', outerWrapperId)
+                .css({
+                    'vertical-align': 'top',
+                    'display': 'block'
+                });
+
+
+        $el.css({
+            'list-style-type': 'none'
+        }).wrap($outerWrapper);
+
+        $lis.css({
+            'display': 'inline-block'
+        }).each(function(i) {
+            widths[i] = $(this).outerWidth() + options.css.border.width * 2;
+            heights[i] = $(this).outerHeight() + options.css.border.width * 2;
+            clones[i] = $(this).children().clone();
+        });
+
+        maxWidth = Math.max.apply(Math, widths);
+        maxHeight = Math.max.apply(Math, heights);
+
+        $wrapper = $('<div />')
+            .width(maxWidth)
+            .height(maxHeight)
+            .css({
+                // Setting transitions
+                '-webkit-transition': 'all ' + options.duration + 'ms ' + options.animation,
+                '-moz-transition': 'all ' + options.duration + 'ms ' + options.animation,
+                '-ms-transition': 'all ' + options.duration + 'ms ' + options.animation,
+                '-o-transition': 'all ' + options.duration + 'ms ' + options.animation,
+                'transition': 'all ' + options.duration + 'ms ' + options.animation,
+
+                // Others properties
+                'float': 'left',
+                'display': 'inline-block',
+                'padding': 0,
+                'position': 'relative',
+                'border': options.css.border.width + 'px solid ' + options.css.border.color,
+                'margin': options.css.margin
+            });
+
+        if(options.css.pointer) {
+            $wrapper.css('cursor', 'pointer');
+        }
+
+
+        for(var i = 0; i < clones.length; ++i) {
+            var title = $lis.eq(i).data('title')
+                , link = $lis.eq(i).data('href')
+                , order = $lis.eq(i).data('order')
+                , $wrapperClone = $wrapper
+                                    .clone()
+                                    .attr('data-id', i)
+                                    .html(clones[i])
+                                    .attr({
+                                        'class': 'sdfm-inner-wrapper ' + $lis.eq(i).attr('class')
+                                    });
+
+            if(typeof(title) !== 'undefined') {
+                $.sdFilterMe.addOverlayTitles($wrapperClone, title, options, maxWidth, maxHeight);
+            }
+            // if(typeof(link) !== 'undefined') {
+            //     $.sdFilterMe.addLink($wrapperClone, link, options);
+            // }
+            if(typeof(order) !== 'undefined') {
+                $wrapperClone.attr('data-order', order);
+            }
+
+            $lis.eq(i).remove();
+            $('#' + outerWrapperId).append($wrapperClone);
+        }
+
+        $el.remove();
+
+        return $('#' + outerWrapperId);
+
+    };
+
+    $.sdFilterMe.storeCoordinates = function($el) {
+
+        var layout = {};
+        $el.find('> div').each(function(i) {
+
+            layout[i] = {
+                origPosX: this.offsetLeft,
+                origPosY: this.offsetTop,
+                newPosX: this.offsetLeft,
+                newPosY: this.offsetTop
+            };
+        });
+        return layout;
+    };
+
+    $.sdFilterMe.addOverlayTitles = function($box, title, options, maxWidth, maxHeight) {
+
+        var backgroundColor = options.css.overlay.background;
+        $overlay = $('<div />')
+                    .addClass('sdfm-overlay')
+                    .css({
+                        'background-color': 'rgba(' + backgroundColor.r + ', ' + backgroundColor.v + ', ' + backgroundColor.b + ', ' + options.css.overlay.opacity + ')',
+                        'position': 'absolute',
+                        'top': 0,
+
+                        '-webkit-transition': 'all ' + options.css.overlay.duration + 'ms ' + options.animation,
+                        '-moz-transition': 'all ' + options.css.overlay.duration + 'ms ' + options.animation,
+                        '-ms-transition': 'all ' + options.css.overlay.duration + 'ms ' + options.animation,
+                        '-o-transition': 'all ' + options.css.overlay.duration + 'ms ' + options.animation,
+                        'transition': 'all ' + options.css.overlay.duration + 'ms ' + options.animation,
+
+                        'text-align': 'center',
+                        'left': 0,
+                        'width': maxWidth - options.css.border.width * 2,
+                        'height': maxHeight - options.css.border.width * 2
+                    });
+
+        $title = $('<span />')
+            .css({
+                'margin': 'auto',
+                'text-transform': 'uppercase',
+                'display': 'inline-block',
+                'padding': '5px',
+                'width': 'auto',
+                'height': '50px',
+                'top': '50%',
+                'margin-top': '25px',
+                'color': options.css.overlay.color,
+                'font-size': '2em',
+                'border': options.css.overlay.border,
+                'font-weight': 'bold'
+            }).html(title);
+
+        $box.append($overlay.append($title));
+    };
+
+    $.sdFilterMe.translateBox = function($box, target, options, hide) {
+        var i = $box.index()
+            , top = $.layout[i].origPosY
+            , left = $.layout[i].origPosX
+            , origPosX = $.layout[target].origPosX
+            , origPosY = $.layout[target].origPosY
+            , translateX = origPosX - left
+            , translateY = origPosY - top
+            , cssValue = 'translate(' + translateX + 'px, ' + translateY + 'px)';
+
+        if(options.sortedOut == 'disappear' && hide === true) {
+            cssValue += ' scale(0, 0)';
+            $box.addClass('sdfm-box-hidden');
+        } else if(options.sortedOut == 'disappear' && hide === false) {
+            cssValue += ' scale(1, 1)';
+            $box.removeClass('sdfm-box-hidden');
+        }
+
+        $.sdFilterMe.applyTransform($box, cssValue);
+
+        $.layout[i].newPosX = origPosX;
+        $.layout[i].newPosY = origPosY;
+    };
+
+    $.sdFilterMe.filterBoxes = function($el, filter, options) {
+
+        var j = 0
+            , $boxes = $el.find('> .sdfm-inner-wrapper')
+            , k = $boxes.length - 1;
+
+        $boxes.each(function() {
+
+            if($(this).hasClass(filter) || filter === '*') {
+
+                if(options.sortedOut == 'opacity') {
+                    $(this).animate({opacity: 1}, {duration: options.duration});
+                }
+
+                $.sdFilterMe.translateBox($(this), j++, options, false);
+            } else {
+
+                if(options.sortedOut == 'opacity') {
+                    $(this).animate({opacity: 0.25}, {duration: options.duration});
+                }
+
+                $.sdFilterMe.translateBox($(this), k--, options, true);
+            }
+        });
+    };
+
+    $.sdFilterMe.applyTransform = function($box, value) {
+        $box.css({
+            '-webkit-transform': value,
+            '-moz-transform': value,
+            '-o-transform': value,
+            '-ms-transform': value,
+            'transform': value
+        });
+    };
+
+    $.sdFilterMe.hoverEffect = function($el, options) {
+
+        $el.find('> .sdfm-inner-wrapper').hover(function() {
+
+            $(this).find('> .sdfm-overlay').fadeOut(options.css.overlay.duration).css({
+                'transform': 'scale(0, 0)'
+            });
+
+        }, function() {
+
+            $(this).find('> .sdfm-overlay').fadeIn(options.css.overlay.duration).css({
+                'transform': 'scale(1, 1)'
+            });
+
+        })
+    };
+
+    $.sdFilterMe.addLink = function($box, link, options) {
+
+        $box.on('click', function() {
+            document.location = link;
+        });
+
+    };
+
+    $.sdFilterMe.defaults = {
+        filterSelector: '.sorter',
+        orderSelector: '.orderer',
+        duration: 1000,
+        animation: 'ease',
+        hoverEffect: true,
+        sortedOut: 'disappear',
+        css: {
+            overlay: {
+                background: {
+                    r: 0,
+                    v: 0,
+                    b: 0
+                },
+                duration: 250,
+                border: '1px solid white',
+                color: 'white',
+                opacity: 0.5
+            },
+            margin: 10,
+            pointer: true,
+            border: {
+                width: 10,
+                color: '#2A4153'
+            }
+        }
+    };
+
+})(jQuery);
